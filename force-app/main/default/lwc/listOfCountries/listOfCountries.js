2be901f94e8ff996756afb881f39f32788c2c4e5
Wazid <m.wazid@ibirdsservices.com>
2026-03-26 18:51:45 +0530

    With Flex and other LWC

---
+++ force-app/main/default/lwc/listOfCountries/listOfCountries.js
@@ -0,0 +1,24 @@
+import {LightningElement,api} from 'lwc';
+import {OmniscriptBaseMixin} from 'omnistudio/omniscriptBaseMixin';
+
+export default class ListOfCountries extends OmniscriptBaseMixin(LightningElement) {
+    @api listCountries = [];
+    setAllCountries;
+    alldata;
+    connectedCallback() {
+        //code
+        this.alldata = JSON.parse(JSON.stringify(this.omniJsonData));
+        this.listCountries = this.alldata.Country;
+       
+        this.setAllCountries = new Set();
+        this.listCountries.forEach(item => this.setAllCountries.add(item))
+        
+    }
+    selectionChangeHandler(event) {		
+        var filteredJSON = this.alldata.Output.Account.filter(item => item.Country == event.target.value);        
+        filteredJSON ='{ "Output": { "Account":'+JSON.stringify(filteredJSON)+'}} ';        
+        this.omniApplyCallResp(JSON.parse(filteredJSON));     
+
+	}
+    
+}
